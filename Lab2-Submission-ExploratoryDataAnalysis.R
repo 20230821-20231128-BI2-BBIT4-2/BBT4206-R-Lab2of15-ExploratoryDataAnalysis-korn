c0be75d1c5b9e18f6be2595a2bf2b2e44d25cc3d
sebTeam4 <sebastian.muramara@strathmore.edu>
2023-09-25 15:12:13 +0300

    upto step 22

--- Lab2-Submission-ExploratoryDataAnalysis.R
+++ Lab2-Submission-ExploratoryDataAnalysis.R
@@ -35,9 +35,7 @@ if (!is.element("readr", installed.packages()[, 1])) {
 require("readr")
 
 
-
-View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset.csv)
-
+View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
 
 ## STEP 4. Load sample datasets that are provided as part of a package ----
 if (!is.element("mlbench", installed.packages()[, 1])) {
@@ -45,7 +43,8 @@ if (!is.element("mlbench", installed.packages()[, 1])) {
 }
 require("mlbench")
 
-data("X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset.csv")
+data("PimaIndiansDiabetes")
+data("BostonHousing")
 
 ## STEP 5. Preview the Loaded Datasets ----
 # Dimensions refer to the number of observations (rows) and the number of
@@ -82,10 +81,10 @@ print(student_performance_romantic_relationships_mode)
 ### STEP 9. Measure the distribution of the data for each variable ----
 summary(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
 #check on this
-sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[,28], sd)
+sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[,21], sd)
 
 ### STEP 11. Measure the variance of each variable ----
-sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[,-1], var)
+sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[,2:55], var)
 
 ### STEP 12. Measure the kurtosis of each variable ----
 # The Kurtosis informs you of how often outliers occur in the results.
@@ -105,3 +104,81 @@ sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, 4],  skewness,
 # only, not categorical values.
 X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cov <- cov(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, 4])
 View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cov)
+
+
+## STEP 15. Measure the correlation between variables ----
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cor <- cor(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, 2:55])
+View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cor)
+
+## STEP 16. Perform ANOVA on the “crop_dataset” dataset ----
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_one_way_anova <- aov(motivator ~ GRADE, data = X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
+summary(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_one_way_anova)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_two_way_anova <- aov(drop_bi_now ~ read_content_before_lecture + study_time, # nolint
+                                           data = X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
+summary(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_two_way_anova)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_two_way_anova <- aov(drop_bi_now ~ read_content_before_lecture * study_time, # nolint
+                                                                              data = X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
+summary(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_two_way_anova)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_two_way_anova <- aov(drop_bi_now ~ read_content_before_lecture + study_time + find_terms_I_do_not_know, # nolint
+                                                                              data = X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
+summary(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_two_way_anova)
+
+### STEP 17. Create Histograms for Each Numeric Attribute ----
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_histo <- as.numeric(unlist(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, 1]))
+hist(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_histo, main = names(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)[1])
+
+### STEP 18. Create Box and Whisker Plots for Each Numeric Attribute ----
+par(mfrow = c(1, 3))
+for (i in 2:10) {
+  boxplot(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, i], main = names(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)[i])
+}
+
+### STEP 19. Create Bar Plots for Each Categorical Attribute ----
+barplot(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, 2]), main = names(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)[2])
+
+### STEP 20. Create a Missingness Map to Identify Missing Data ----
+if (!is.element("Amelia", installed.packages()[, 1])) {
+  install.packages("Amelia", dependencies = TRUE)
+}
+require("Amelia")
+missmap(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset, col = c("red", "grey"), legend = TRUE)
+
+data(Soybean)
+View(Soybean)
+missmap(Soybean, col = c("red", "grey"), legend = TRUE)
+
+### STEP 21. Create a Correlation Plot ----
+if (!is.element("ggcorrplot", installed.packages()[, 1])) {
+  install.packages("ggcorrplot", dependencies = TRUE)
+}
+require("ggcorrplot")
+ggcorrplot(cor(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, 2:4]))
+
+### STEP 22. Create a Scatter Plot ----
+ggplot(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset,
+       aes(x = YOB, y = motivator, shape = GRADE, color = GRADE)) +
+  geom_point() +
+  geom_smooth(method = lm)
+
+### STEP 23. Create Multivariate Box and Whisker Plots by Class ----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
